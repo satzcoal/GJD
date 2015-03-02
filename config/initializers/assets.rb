41251c6a5167e0bda811d79a414dacb91df18b30
lee <satzcoal@hotmail.com>
2015-03-03 02:20:43 +0800

    2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,6 @@
 Rails.application.config.assets.precompile += %w( origin.js )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.eot )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.ttf )
+Rails.application.config.assets.precompile += %w( fontawesome-webfont.svg )
+Rails.application.config.assets.precompile += %w( FontAwesome.otf )
